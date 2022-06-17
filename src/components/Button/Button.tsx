e27a4f1b09edc9c3241b4ff7b1510656484e0a56
justtoconfirm <office@justtoconfirm.co.uk>
2022-06-17 15:34:03 +0100

    Update the Button component to allow the button to be rendered either as a link or button element

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,32 @@
 import React, { FC } from "react";
 
 export interface ButtonProps {
-	type: "button" | "submit" | "reset" | undefined,
+	type?: "button" | "submit" | "reset" | undefined,
+	url?: string,
 	onClick?: () => void,
 	size?: "small" | "medium" | "large",
 	disabled?: boolean,
 	children: string
 };
 
-const Button: FC<ButtonProps> = ({ type, size, onClick, disabled, children, ...props }) => {
+const Button: FC<ButtonProps> = ({ type, url, onClick, size, disabled, children, ...props }) => {
+
+	// Check if the component has a url prop value that matches those listed
+	const isLink = url && (url.includes("http") || url.startsWith("#") || url.startsWith("mailto") || url.startsWith("/"));
+
+	const renderAsLink = () =>
+		<a href={url} role="button">{children}</a>
+
+	const renderAsButton = () =>
+	<button {...{ type, size, onClick, disabled }}>{children}</button>
+
 	return (
-		<button {...{ type, size, onClick, disabled }}>{children}</button>
+		/*
+			If 'url' prop is passed into the component with a value,
+			render as a link by calling the renderAsLink() method, otherwise,
+			render as a button by calling the renderAsButton() method.
+		*/
+		isLink ? renderAsLink() : renderAsButton()
 	)
 };
 
