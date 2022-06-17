e4652b27e74c8aa2ab8bec416757a3bf0de2d534
Thetinywebagency <104509822+ttwebagency@users.noreply.github.com>
2022-06-17 15:44:16 +0100

    Update the Button component with props and initial CSS styling (#6)
    * Update Button component with props and type checking
    
    * Pass the ButtonProps interface object into the button component
    
    * Correct position of semi-colon within the button component code
    
    * Update the README file
    
    * Update the Button component to allow the button to be rendered either as a link or button element
    
    * Add initial Button styling using styled-components
    
    * Code comment added
    
    * README file updated
    
    Co-authored-by: justtoconfirm <office@justtoconfirm.co.uk>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const ButtonStyle = styled.button`
+	display: inline-block;
+	text-decoration: none;
+	text-align: center;
+`;
 
 export interface ButtonProps {
-	label: string;
-}
+	type?: "button" | "submit" | "reset" | undefined,
+	url?: string,
+	onClick?: () => void,
+	size?: "small" | "medium" | "large",
+	disabled?: boolean,
+	children: string
+};
 
-const Button = (props: ButtonProps) => {
-	return <button>{props.label}</button>
+const Button: FC<ButtonProps> = ({ type, url, onClick, size, disabled, children, ...props }) => {
+
+	// Check if the component has a url prop value that matches those listed
+	const isLink = url && (url.includes("http") || url.startsWith("#") || url.startsWith("mailto") || url.startsWith("/"));
+
+	// This method will render a link styled to appear as a button
+	const renderAsLink = () =>
+		<a href={url} role="button">{children}</a>
+
+	// This method will render a <button> element
+	const renderAsButton = () =>
+	<ButtonStyle {...{ type, size, onClick, disabled }}>{children}</ButtonStyle>
+
+	return (
+		/*
+			If 'url' prop is passed into the component with a value,
+			render as a link by calling the renderAsLink() method, otherwise,
+			render as a button by calling the renderAsButton() method.
+		*/
+		isLink ? renderAsLink() : renderAsButton()
+	)
 };
 
 export default Button;
