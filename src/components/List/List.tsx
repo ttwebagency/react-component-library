ba25364a996c54bb2ebc145790a44d0f3862ddc2
Thetinywebagency <104509822+ttwebagency@users.noreply.github.com>
2022-06-20 16:26:22 +0100

    List component (#13)
    * List component added
    
    * Export the List component
    
    * Missing semicolon added
    
    Co-authored-by: justtoconfirm <office@justtoconfirm.co.uk>

---
+++ src/components/List/List.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react";
+
+export interface ListProps {
+    type: "ul" | "ol",
+    list: []
+};
+
+const List: FC<ListProps> = ({ type, list }) => {
+
+    // Loop over an object and map to an individual item in the list with a unique key
+    const listItems = list.map(( item, index ) =>
+        <li key={index}>{item}</li>
+    );
+
+    // Check the list type prop value to render as an ordered list
+    const listType = type && (type.includes("ol"));
+
+    // This method will render the list as an ordered list
+    const renderOrderedList = () =>
+        <ol>
+            {listItems}
+        </ol>
+
+    // This method will render the list as an unordered list
+    const renderUnorderedList = () =>
+        <ul>
+            {listItems}
+        </ul>
+
+    return (
+        /*
+            If 'type' prop has the value:
+            'ol', an ordered list will be rendered,
+            'ul', an unordered list will be rendered.
+        */
+        listType ? renderOrderedList() : renderUnorderedList()
+    );
+};
+
+export default List;
