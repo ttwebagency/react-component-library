4e07ae5c8bfc358fdce2920c0f7c92392d4a730b
Thetinywebagency <104509822+ttwebagency@users.noreply.github.com>
2022-06-21 16:24:10 +0100

    Amendment to the component exports (#15)
    Co-authored-by: justtoconfirm <office@justtoconfirm.co.uk>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from "./Button";
-export { default as Label } from "./Label";
-export { default as Layout } from "./Layout";
-export { default as List } from "./List";
+export * as Button from "./Button";
+export * as Label from "./Label";
+export * as Layout from "./Layout";
+export * as List from "./List";
