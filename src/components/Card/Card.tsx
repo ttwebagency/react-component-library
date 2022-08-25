fb62adcbc0dc8a6b2ec3247fc574076575ba28f7
Thetinywebagency <104509822+ttwebagency@users.noreply.github.com>
2022-08-25 16:17:34 +0100

    Add the Card component to the component library (#20)
    * Initial Card component code with export added to add to the component library
    
    * Add children prop to the Card component
    
    * Add initial Card component styling
    
    * Update README file
    
    Co-authored-by: justtoconfirm <office@justtoconfirm.co.uk>

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const CardStyling = styled.div`
+    border: 1px solid #eee;
+    border-radius: 4px;
+    width: 100%;
+`;
+
+const CardInnerStyling = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    padding: 2rem;
+    height: 100%;
+    justify-content: space-between;
+`;
+
+const CardContentStyling = styled.div`
+    flex-grow: 1;
+`;
+
+export interface CardProps {
+    children: string
+};
+
+export const Card: FC<CardProps> = ({ children }) => {
+
+    return (
+        <CardStyling>
+            <CardInnerStyling>
+                <CardContentStyling>
+                    {children}
+                </CardContentStyling>
+            </CardInnerStyling>
+        </CardStyling>        
+    )
+};
