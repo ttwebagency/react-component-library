df1b0205df1e8806c4686358d0ca4be579efeb75
justtoconfirm <office@justtoconfirm.co.uk>
2022-09-26 16:53:49 +0100

    Add the isRow prop to the Grid component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -3,14 +3,16 @@ import styled from "styled-components";
 
 const GridStyles = styled.div`
     display: flex;
+    flex-direction: ${props => props.isRow ? "row" : "column"};
 `;
 
 export interface GridProps {
     children: any,
+    isRow: boolean
 }
 
-export const Grid: FC<GridProps> = ({ children }) => (
-    <GridStyles>
+export const Grid: FC<GridProps> = ({ isRow = false, children }) => (
+    <GridStyles {...{isRow}}>
         {children}
     </GridStyles>
 );
