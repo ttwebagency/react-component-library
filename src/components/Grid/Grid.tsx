322e6bf63b647c86da09af95fb415463b4aa7bb3
justtoconfirm <office@justtoconfirm.co.uk>
2022-09-26 17:09:32 +0100

    Fix applied to pass in props into styled-components for the Grid component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 
-const GridStyles = styled.div`
+const GridStyles = styled("div")<GridProps>`
     display: flex;
+    flex-direction: ${props => props.isRow ? "row" : "column"};
 `;
 
 export interface GridProps {
     children: any,
-    isRow: boolean
+    isRow?: boolean
 }
 
 export const Grid: FC<GridProps> = ({ isRow = false, children }) => (
