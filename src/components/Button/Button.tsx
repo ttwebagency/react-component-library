58215c2c9558e9b50198e868ee9238aa0a141ca1
justtoconfirm <office@justtoconfirm.co.uk>
2022-09-26 16:13:10 +0100

    Remove unused spread operator

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -28,7 +28,7 @@ export interface ButtonProps {
 	children: string
 };
 
-export const Button: FC<ButtonProps> = ({ type, url, onClick, size, target, disabled, children, ...props }) => {
+export const Button: FC<ButtonProps> = ({ type, url, onClick, size, target, disabled, children }) => {
 
 	// Check if the component has a url prop value that matches those listed
 	const isLink = url && (url.includes("http") || url.startsWith("#") || url.startsWith("mailto") || url.startsWith("/"));
