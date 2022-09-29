cd1cc152aeabab2e45aae12baaf46dbfae16fdb9
justtoconfirm <office@justtoconfirm.co.uk>
2022-09-29 13:20:43 +0100

    Allow Image component to render within a link

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -4,12 +4,25 @@ export type ImageProps = {
     src: string,
     height: string,
     width: string,
-    alt: string
+    alt: string,
+    url?: string,
+    openAsTab?: boolean,
+    lazyLoad?: boolean
 }
 
-export const Image = ({ src, height, width, alt }: ImageProps) => {
+export const Image = ({ src, height, width, alt, url, openAsTab, lazyLoad }: ImageProps) => {
 
+    const isLink = url && (url.includes('http') || url.startsWith('#') || url.startsWith('mailto') || url.startsWith('/'));
+
+    const renderLink = () =>
+        <a href={url} target={openAsTab ? 'blank' : undefined}>
+            <img {...{ src, height, width, alt }} loading={lazyLoad ? 'lazy' : 'eager' } />
+        </a>
+
+    const renderImage = () =>
+        <img {...{ src, height, width, alt}} loading={lazyLoad ? 'lazy' : 'eager'} />
+    
     return (
-        <img {...{ src, height, width, alt }} />
-    )
+        isLink ? renderLink() : renderImage()
+    );
 };
